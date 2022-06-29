c5ce4fdfae2cab6646c139f4a7feb2022ba4d389
Saurav Rijal <sauravrijal1011@gmail.com>
2022-06-29 08:42:25 +0545

    Page not found error

---
+++ mysite/blog/urls.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+app_name='blog'
+
+urlpatterns=[
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+]
